1ee97b42cc6b42ffe2fe050027ddd7f7a9adb621
Guy Davis <guydavis.ca@gmail.com>
2022-05-10 13:53:50 -0600

    Ignore spurious offline alerts on startup.

--- src/notifier/__init__.py
+++ src/notifier/__init__.py
@@ -4,6 +4,7 @@
 import json
 import logging
 import re
+import time
 import traceback
 
 # std
@@ -13,6 +14,8 @@ from json_logic import jsonLogic
 from typing import List
 from enum import Enum
 
+# Ignore Chiadog alerts about being offline due to entire container just launching in the first 30 minutes
+MINIMUM_LAUNCH_SECONDS_BEFORE_ALERTING_ABOUT_BEING_OFFLINE = 30 * 60
 
 class EventPriority(Enum):
     """Event priority dictates how urgently
@@ -66,6 +69,7 @@ class Notifier(ABC):
     """
 
     def __init__(self, title_prefix: str, config: dict):
+        self._program_launch_time = time.time()
         self._title_prefix = title_prefix
         self._config = config
         self._conn_timeout_seconds = 10
@@ -98,10 +102,18 @@ class Notifier(ABC):
         return f"{icon} {self._title_prefix} {event.service.name}"
 
     def should_ignore_event(self, event):
+        # Automatically ignore Chiadog's spurious "Your harvester appears to be offline!" alerts immediately after a relaunch of container
+        # Obviously if the Machinaris container (and thus all farming/harvesting) was just started, there will be a gap in the log... 
+        if (self._program_launch_time + MINIMUM_LAUNCH_SECONDS_BEFORE_ALERTING_ABOUT_BEING_OFFLINE) >= time.time():
+            if (event.service.name == 'HARVESTER' and event.message.startswith("Your harvester appears to be offline!")) or \
+                (event.service.name == 'FULL_NODE' and event.message.startswith("Experiencing networking issues?")):
+                return True
+        # Next only ignore if user has set an "ignore" clause in config.xml for a particular Notifier
         if not "ignore" in self._config:
             return False 
         ignore = self._config["ignore"]
         try:
+            # First check for one of type, priority, service, and message as a simple filter
             if 'type' in ignore and ignore['type'] == event.type.name:
                 return True
             if 'priority' in ignore and ignore['priority'] == event.priority.name:
@@ -110,6 +122,7 @@ class Notifier(ABC):
                 return True
             if 'message' in ignore and re.search(ignore['message'], event.message, re.M|re.I):
                 return True
+            # Then look for compound ignore clause to invoke json logic
             if 'compound' in ignore:
                 rule = json.loads(ignore['compound'])
                 data = {   
